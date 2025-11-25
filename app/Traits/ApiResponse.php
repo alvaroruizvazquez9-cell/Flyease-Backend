9969a9b9a798a9e5468ebce4a09816827029ae15
Alvaro <alvaroruizvazquez9@gmail.com>
2025-11-25 20:12:28 +0100

    intento de resolver autenticacion

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    /**
+     * Return a success JSON response.
+     *
+     * @param mixed $data
+     * @param string $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function success($data = null, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    /**
+     * Return an error JSON response.
+     *
+     * @param string $message
+     * @param int $code
+     * @param mixed $errors
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function error($message = 'Error', $code = 400, $errors = null)
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+        ];
+
+        if ($errors !== null) {
+            $response['errors'] = $errors;
+        }
+
+        return response()->json($response, $code);
+    }
+}
